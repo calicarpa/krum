c0a7c57bb6fe415b59dcd0b2f1fa75a2455f8453
Arthur DANJOU <arthurdanjou@outlook.fr>
2026-04-29 14:19:59 +0200

    Deleted untracked files

--- docs/_build/html/_static/custom-icon.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('.viewcode-link, .reference.external[href*="github.com"]').forEach(function (link) {
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener noreferrer");
-  });
-});
